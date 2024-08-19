5de95ae3cd885ce0d7e12f019d142e89cff4e86c
Zdvihal Vaclav <Vaclav.Zdvihal@newtonmedia.cz>
2024-08-19 17:23:54 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import ArticleCard from "./components/ArticleCard.vue";
+import {ref} from "vue";
+
+const hideHeader = ref(false);
+
+function scrollHandler(details) {
+  console.log(details);
+  hideHeader.value = details.direction === 'down';
+}
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+
+        <div class="n-search-layout">
+          <header v-if="!hideHeader"
+                  :class="hideHeader ? 'animate__fadeOut' : 'animate__fadeIn'"
+                  class="n-search-layout__header bg-orange-4 animate__animated">
+            <div class=" q-px-md q-py-sm row q-gutter-sm">
+              <q-badge color="purple" v-for="n in 30">Lorem ipsum dolor.</q-badge>
+            </div>
+          </header>
+          <header class="n-search-layout__header-stay bg-pink-4">
+            <div class=" q-px-md q-py-sm row q-gutter-sm">
+              <q-badge v-for="n in 60">Lorem ipsum.</q-badge>
+            </div>
+          </header>
+
+          <div class="n-search-layout__content bg-green-4">
+            <q-scroll-area class="fit q-px-lg">
+                <q-scroll-observer @scroll="scrollHandler" />
+                <ArticleCard class="q-my-md" v-for="n in 30" />
+            </q-scroll-area>
+          </div>
+        </div>
+  </q-layout>
+</template>
+
+<style scoped lang="scss">
+.n-search-layout {
+  display: grid;
+  grid-template-rows: auto auto 1fr;
+  height: 100vh;
+}
+.n-search-layout__header {
+  grid-area: 1 / 1 / 2 / 3;
+}
+.n-search-layout__header-stay {
+  grid-area: 2 / 1 / 3 / 3;
+}
+.n-search-layout__content {
+  grid-area: 3 / 1 / 4 / 3;
+}
+</style>
---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <q-card class="row">
+    <q-card-section class="col-auto">
+      <q-img
+        src="https://picsum.photos/200"
+        style="height: 200px; width: 200px"
+        basic
+        contain
+      />
+    </q-card-section>
+    <q-card-section class="col">
+      <div class="column">
+        <span class="text-h6">Some article</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet consequuntur cupiditate deleniti doloremque eaque earum, eveniet fugit id nemo pariatur porro quasi repudiandae sed voluptatem! Culpa magni suscipit ullam.
+        </p>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
